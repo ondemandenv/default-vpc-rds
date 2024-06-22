2c31041778d6b106793b92c1f61400c1b5e9ef1d
jj <jj@ondemandenv.dev>
2024-06-22 12:06:48 -0400

    move lambda to central

--- lib/repo-build-ctl-vpc-rds.ts
+++ lib/repo-build-ctl-vpc-rds.ts
@@ -1,32 +1,27 @@
 import {Port, SecurityGroup, SelectedSubnets, SubnetType} from "aws-cdk-lib/aws-ec2";
 import {RepoBuildCtlVpc} from "./repo-build-ctl-vpc";
 import {
-    AuroraPostgresEngineVersion, CfnDBCluster,
+    CfnDBCluster,
     Credentials,
-    DatabaseClusterEngine,
     ParameterGroup,
     ServerlessCluster
 } from "aws-cdk-lib/aws-rds";
-import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
-import {Policy, PolicyStatement} from "aws-cdk-lib/aws-iam";
-import {Runtime} from "aws-cdk-lib/aws-lambda";
-import * as path from "node:path";
-import {App, CfnOutput, Duration, Stack, StackProps} from "aws-cdk-lib";
-import {RetentionDays} from "aws-cdk-lib/aws-logs";
-import {Provider} from "aws-cdk-lib/custom-resources";
+import {App, Stack, StackProps} from "aws-cdk-lib";
 import {RepoBuildCtlVpcRdsSchusrs} from "./repo-build-ctl-vpc-rds-schusrs";
 import {
     AnyContractsEnVer,
     ContractsCrossRefProducer, ContractsEnverCdk,
-    ContractsRdsCluster, ContractsShareOut,
-    GET_PG_USR_ROLE_PROVIDER_NAME
+    ContractsRdsCluster, ContractsShareOut, OndemandContracts,
 } from "@ondemandenv/odmd-contracts";
+import {
+    ContractsEnverCdkDefaultVpc
+} from "@ondemandenv/odmd-contracts/lib/repos/_default-vpc-rds/odmd-enver-default-vpc-rds";
+import {ArnPrincipal, Policy, PolicyStatement, Role} from "aws-cdk-lib/aws-iam";
 
 
 export class RepoBuildCtlVpcRds extends Stack {
 
     private readonly vpcStack: RepoBuildCtlVpc
-    private readonly schUsrFun: NodejsFunction
     private readonly rdsCluster: ServerlessCluster;
 
     constructor(parent: App, vpcStack: RepoBuildCtlVpc, rds: ContractsRdsCluster, props: StackProps) {
@@ -48,10 +43,10 @@ export class RepoBuildCtlVpcRds extends Stack {
             securityGroupName: rds.defaultSgName,
             vpc: vpcStack.vpc,
         });
-        const engine = DatabaseClusterEngine.auroraPostgres({version: AuroraPostgresEngineVersion.VER_13_12});
+
         this.rdsCluster = new ServerlessCluster(this, rds.clusterIdentifier, {
             clusterIdentifier: rds.clusterIdentifier,
-            engine,
+            engine: rds.engine,
             vpc: vpcStack.vpc,
             scaling: rds.scaling,
             defaultDatabaseName: rds.defaultDatabaseName,
@@ -59,8 +54,8 @@ export class RepoBuildCtlVpcRds extends Stack {
             vpcSubnets: rdsSubnets,
             credentials: Credentials.fromGeneratedSecret(rds.rootUsername, {secretName: rds.rootSecretName}),
             parameterGroup: new ParameterGroup(this, 'paramGroup', {
-                engine,
-                parameters:{
+                engine: rds.engine,
+                parameters: {
                     log_connections: '1',
                     log_disconnections: '1',
                     log_lock_waits: '1',
@@ -72,44 +67,25 @@ export class RepoBuildCtlVpcRds extends Stack {
         const cfnCluster = this.rdsCluster.node.defaultChild as CfnDBCluster;
 
         //Aurora Serverless currently doesn't support CloudWatch Log Export.
-        cfnCluster.enableCloudwatchLogsExports = ['postgresql'];
-        cfnCluster.performanceInsightsEnabled = true;
+        // cfnCluster.enableCloudwatchLogsExports = ['postgresql'];
+        // cfnCluster.performanceInsightsEnabled = true;
 
         const usrFuncSg = new SecurityGroup(this, 'usr-fun-sg', {
             vpc: vpcStack.vpc,
             securityGroupName: pid + '-pg-schm-usr-fun-sg'
         });
-        /*
-                        adminSecretId: rds.credsSecret.secretName,
-                        postgresHostname: rdsCluster.clusterEndpoint.hostname,
-                        postgresHostport: rdsCluster.clusterEndpoint.port,
-                        databaseName: rds.defaultDatabaseName,*/
-        const secretPath = 'odmd-/' + rds.vpc.build.buildId + '/' + rds.clusterIdentifier;
-        this.schUsrFun = new NodejsFunction(this, 'pg-usr-fun', {
-            memorySize: 256,
-            vpcSubnets: vpcStack.privateSubnets,
-            vpc: vpcStack.vpc,
-            securityGroups: [usrFuncSg],
-            bundling: {
-                externalModules: [
-                    '@aws-sdk/client-secrets-manager'
-                ],
-            },
-            environment: {
-                NODE_OPTIONS: '--unhandled-rejections=strict',
-                adminSecretId: rds.rootSecretName,
-                postgresHostname: this.rdsCluster.clusterEndpoint.hostname,
-                postgresHostport: this.rdsCluster.clusterEndpoint.port.toString(),
-                databaseName: rds.defaultDatabaseName,
-                secretPath
-            },
-            runtime: Runtime.NODEJS_18_X,
-            entry: path.join(__dirname, `pg-schema-role-user-cs/index.ts`),
-            timeout: Duration.minutes(3),
-            logRetention: RetentionDays.ONE_MONTH
-        });
+
         rdsClusterSg.addIngressRule(usrFuncSg, Port.tcp(this.rdsCluster.clusterEndpoint.port))
-        this.rdsCluster.secret!.grantRead(this.schUsrFun)
+        const myEnver = OndemandContracts.inst.defaultVpcRds.envers.find(e => e.targetRevision.toString() == OndemandContracts.REV_REF_value) as ContractsEnverCdkDefaultVpc;
+
+        const masterRole = new Role(this, 'masterRole', {
+            assumedBy: new ArnPrincipal(`arn:aws:iam::${OndemandContracts.inst.accounts.central}:role/${
+                myEnver.rdsTrustCentralRoleName
+            }`)
+        });
+        this.rdsCluster.secret!.grantRead(masterRole)
+
+        const secretPath = 'odmd/' + rds.vpc.build.buildId + '/' + rds.clusterIdentifier;
         const createUsrPolicy = new Policy(this, 'create-user-secret', {
             statements: [
                 new PolicyStatement({
@@ -118,23 +94,14 @@ export class RepoBuildCtlVpcRds extends Stack {
                 })
             ]
         });
-        this.schUsrFun.role!.attachInlinePolicy(createUsrPolicy)
-        this.schUsrFun.node.addDependency(rdsClusterSg)
-        const provider = new Provider(this, `usr-fun-provider`, {onEventHandler: this.schUsrFun});
-        provider.node.addDependency(this.rdsCluster, createUsrPolicy)
-
-        const exportPgUsrProviderName = GET_PG_USR_ROLE_PROVIDER_NAME(rds.vpc.build.buildId, process.env.CDK_DEFAULT_REGION!,
-            this.account, rds.vpc.vpcName)
-        new CfnOutput(this, exportPgUsrProviderName, {
-            exportName: exportPgUsrProviderName,
-            value: provider.serviceToken
-        })
+        masterRole.attachInlinePolicy(createUsrPolicy)
 
         new ContractsShareOut(this, new Map<ContractsCrossRefProducer<AnyContractsEnVer>, string | number>(
             new Map<ContractsCrossRefProducer<AnyContractsEnVer>, string | number>([
                 [rds.clusterHostname, this.rdsCluster.clusterEndpoint.hostname],
                 [rds.clusterPort, this.rdsCluster.clusterEndpoint.port],
                 [rds.clusterSocketAddress, this.rdsCluster.clusterEndpoint.socketAddress],
+                [rds.clusterMasterRoleArn, masterRole.roleArn],
             ])
         ))
 
